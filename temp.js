1a62f5a839e59a2550f15c787eb8532ac0d78f31
uforever <38533204+uforever@users.noreply.github.com>
2024-09-27 18:08:21 +0800

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,7 +1,7 @@
-const targetLib = "libnative-lib.so";
+const libName = "native-lib";
+const targetLib = `lib${libName}.so`;
 
 function main() {
-    // 先通过dlopen探索具体是哪个so文件在反frida调试
     const adeAddr = Module.findExportByName(null, "android_dlopen_ext");
     Interceptor.attach(adeAddr, {
         onEnter: function (args) {
@@ -16,63 +16,119 @@ function main() {
             }
         },
         onLeave: function () {
-            // 对这个so文件的符号进行hook 看看具体是哪个函数引起的崩溃
             if (this.isTarget) {
-                const baseAddr = Module.findBaseAddress(targetLib);
-                console.log("[dylib base address]: ", baseAddr);
+                // const baseAddr = Module.findBaseAddress(targetLib);
+                // console.log("[dylib base address]: ", baseAddr);
 
-                // 查看是否有新的线程被创建
-                Interceptor.attach(Module.findExportByName("libc.so", "pthread_create"), {
-                    onEnter(args) {
-                        const threadFuncAddr = args[2];
-                        console.log("The thread function address is " + ptr(threadFuncAddr).sub(baseAddr));
+                // // 这里 libcrypto.so 就是 openssl 库
+                // const opensslAddr = Module.findBaseAddress("libcrypto.so");
+                // console.log("[openssl base address]: ", opensslAddr);
 
-                        // const pthreadFunc = args[2];
-                        // try {
-                        //     const module = Process.getModuleByAddress(pthreadFunc);
-                        //     if (module.name === targetLib) {
-                        //         console.log("pthread create by target lib, func addr: ", pthreadFunc);
-                        //         // 替换反调试函数
-                        //         Interceptor.replace(pthreadFunc, new NativeCallback(function () {
-                        //             console.log("bypass anti-debug function");
-                        //             return 0;
-                        //         }, 'int', []));
-                        //     }
-                        // } catch (_e) {
-                        // }
-                    }
-                })
-
-                // 以JNI_OnLoad为例
-                const jniOnload = Module.findExportByName(targetLib, "JNI_OnLoad");
-                console.log("[hit JNI_OnLoad]: " + jniOnload);
-                // 如果有输出的话 说明检测点在JNI_OnLoad之中或者之后
-                // 否则可能在.init_proc .init_array .init_xxx等函数中
-                Interceptor.attach(jniOnload, {
-                    onEnter: function (_args) {
-                        console.log("[func invoke]: JNI_OnLoad");
+                // 导入的函数可以直接在这里hook
+                Interceptor.attach(Module.findExportByName(targetLib, "EVP_EncryptUpdate"), {
+                    onEnter: function (args) {
+                        this.out = args[1];
+                        this.outl = args[2];
+                        const inl = args[4].toUInt32();
+                        console.log("[onEnter] EVP_EncryptUpdate");
+                        console.log(`input(${inl}):\n${hexdump(args[3],
+                            {
+                                offset: 0,
+                                length: inl,
+                                header: true,
+                                ansi: true
+                            }
+                        )}\n`);
+                    },
+                    onLeave: function (_retval) {
+                        console.log("[onLeave] EVP_EncryptUpdate");
+                        const outl = ptr(this.outl).readInt();
+                        console.log(`output(${outl}):\n${hexdump(this.out, {
+                            offset: 0,
+                            length: outl,
+                            header: true,
+                            ansi: true
+                        })}\n`);
                     },
                 });
 
-                // 目标函数名
-                const funcAddr = Module.findExportByName(targetLib, "Java_com_r0ysue_test1_MainActivity_stringFromJNI");
-                console.log("[hit target func]: " + funcAddr);
-                Interceptor.attach(funcAddr, {
-                    onEnter: function (_args) {
-                        console.log("[func invoke]: Java_com_r0ysue_test1_MainActivity_stringFromJNI");
+                Interceptor.attach(Module.findExportByName(targetLib, "EVP_EncryptFinal"), {
+                    onEnter: function (args) {
+                        this.out = args[1];
+                        this.outl = args[2];
                     },
-                })
+                    onLeave: function (_retval) {
+                        console.log("[onLeave] EVP_EncryptFinal");
+                        const outl = ptr(this.outl).readInt();
+                        console.log(`output(${outl}):\n${hexdump(this.out, {
+                            offset: 0,
+                            length: outl,
+                            header: true,
+                            ansi: true
+                        })}\n`);
+                    },
+                });
 
+                Interceptor.attach(Module.findExportByName(targetLib, "EVP_EncodeUpdate"), {
+                    onEnter: function (args) {
+                        console.log("[onEnter] EVP_EncodeUpdate");
+                        const inl = args[4].toUInt32();
+                        console.log(`input(${inl}):\n${hexdump(args[3],
+                            {
+                                offset: 0,
+                                length: inl,
+                                header: true,
+                                ansi: true
+                            }
+                        )}\n`);
+                    },
+                });
+
+                Interceptor.attach(Module.findExportByName(targetLib, "EVP_EncodeFinal"), {
+                    onEnter: function (args) {
+                        this.out = args[1];
+                        this.outl = args[2];
+                    },
+                    onLeave: function (_retval) {
+                        console.log("[onLeave] EVP_EncodeFinal");
+                        console.log(`output(${ptr(this.outl).readInt()}): ${this.out.readCString()}\n`);
+                    },
+                });
+
+                // 未被导入的函数仍需要通过libcrypto.so来hook
+                Interceptor.attach(Module.findExportByName("libcrypto.so", "AES_set_encrypt_key"), {
+                    onEnter: function (args) {
+                        console.log("[onEnter] AES_set_encrypt_key");
+                        this.keyLen = args[1].toInt32() / 8;
+                        this.aesKey = args[2];
+                        console.log(`input: ${args[0].readCString()}\n`);
+                    },
+                    onLeave: function (_retval) {
+                        console.log("[onLeave] AES_set_encrypt_key");
+                        console.log(`output(${this.keyLen}):\n${hexdump(this.aesKey, {
+                            offset: 0,
+                            length: this.keyLen,
+                            header: true,
+                            ansi: true
+                        })}\n`);
+                    }
+                });
+
+                Interceptor.attach(Module.findExportByName("libcrypto.so", "AES_encrypt"), {
+                    onEnter: function (args) {
+                        console.log("[onEnter] AES_encrypt");
+                        const aesKey = args[2];
+                        console.log(`AES_KEY: ${hexdump(aesKey, {
+                            offset: 0,
+                            length: 16,
+                            header: true,
+                            ansi: true
+                        })}\n`);
+                    },
+                });
             }
         }
     });
-
-    // Java.perform(() => {
-    //     const MainActivity = Java.use("com.r0ysue.test1.MainActivity");
-    //     MainActivity.mystr.implementation = function () {
-    //         return true;
-    //     }
-    // });
 }
 
 setImmediate(main);
