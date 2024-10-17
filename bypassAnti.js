dd298ec2305d454e87ccf14be5026ee6866354b0
uforever <38533204+uforever@users.noreply.github.com>
2024-10-17 10:26:00 +0800

    Update bypassAnti.js

--- bypassAnti.js
+++ bypassAnti.js
@@ -2,7 +2,6 @@ const targetLib = "libmsaoaidsec.so";
 let alreadyHook = false;
 let classFactory = null;
 
-// TODO: doJavaHook
 function main() {
   const adeAddr = Module.findExportByName(null, "android_dlopen_ext");
   Interceptor.attach(adeAddr, {
@@ -10,17 +9,51 @@ function main() {
       const pathptr = args[0];
       if (pathptr) {
         const path = ptr(pathptr).readCString();
-        console.log("[dylib open]: ", path);
+        // 分析前先打开
+        // console.log("[dylib open]: ", path);
 
         if (path.includes(targetLib)) {
+          this.isTarget = true;
           hook_init_proc();
         }
       }
     },
+    onLeave: function () {
+      if (this.isTarget) {
+        const jniOnload = Module.findExportByName(targetLib, "JNI_OnLoad");
+        console.log("[hit JNI_OnLoad]: " + jniOnload);
+        // 如果有输出的话 说明检测点在JNI_OnLoad之中或者之后
+        // 否则可能在.init_proc .init_array .init_xxx等函数中
+        Interceptor.attach(jniOnload, {
+          onEnter: function (_args) {
+            // 其中有检测是否有java层hook
+            // hook后 & 0x80000 != 0
+            console.log("[func invoke]: JNI_OnLoad");
+          },
+          onLeave: function () {
+            if (Java.available) {
+              Java.perform(doJavaHook);
+            }
+          },
+        });
+      }
+    },
   });
+}
 
-  // if(Java.available) {
-  // }
+function doJavaHook() {
+  const Application = Java.use("android.app.Application");
+  Application.attach.overload("android.content.Context").implementation = function (context) {
+    this.attach(context);
+    const classLoader = context.getClassLoader();
+    classFactory = Java.ClassFactory.get(classLoader);
+  };
+  if (classFactory) {
+    console.log("[with shell]");
+  } else {
+    classFactory = Java;
+    console.log("[without shell]");
+  }
 }
 
 function hook_init_proc() {
@@ -51,6 +84,15 @@ function hook_before_init_proc(targetSo) {
   const baseAddr = targetSo.base;
   console.log("targetSo.base: " + baseAddr);
 
+  // 获取函数hook之前的前8个字节
+  // const xxxPtr = Module.findExportByName("libc.so", "xxx");
+  // console.log(`access first 8 bytes before hook: ${hexdump(xxxPtr, {
+  //   offset: 0,
+  //   length: 8,
+  //   header: true,
+  //   ansi: true
+  // })}`);
+
   // 分析前先注释掉这里
   nop(baseAddr, 0x1C544);
   nop(baseAddr, 0x1B8D4);
@@ -58,13 +100,15 @@ function hook_before_init_proc(targetSo) {
 
   generalBypassHook();
 
+  // 分析前先打开这里 注释掉上面
   // hook pthread_create 函数
-  Interceptor.attach(Module.findExportByName("libc.so", "pthread_create"), {
-    onEnter(args) {
-      const threadFuncAddr = args[2];
-      console.log("The thread function address is " + ptr(threadFuncAddr).sub(baseAddr));
-    }
-  });
+  // Interceptor.attach(Module.findExportByName("libc.so", "pthread_create"), {
+  //   onEnter(args) {
+  //     const threadFuncAddr = args[2];
+  //     console.log("The thread function address is " + ptr(threadFuncAddr).sub(baseAddr));
+  //   }
+  // });
+
   /*
   [dylib open]:  /system/framework/oat/arm64/org.apache.http.legacy.odex
   [dylib open]:  /data/app/~~Hu9R_ySuFoCUOt4uZED-ig==/cn.soulapp.android-V18oODwiM_xA47Z6dBWmaQ==/oat/arm64/base.odex
@@ -90,7 +134,7 @@ function nop(base, offset) {
 
 function generalBypassHook() {
   // hook fgets 函数
-  const fgetsPtr = Module.findExportByName("libc.so", "fgets");
+  const fgetsPtr = Module.findExportByName("libc.so", 'fgets');
   const fgets = new NativeFunction(fgetsPtr, 'pointer', ['pointer', 'int', 'pointer']);
   Interceptor.replace(fgetsPtr, new NativeCallback(function (buffer, size, fp) {
     const retval = fgets(buffer, size, fp);
@@ -103,34 +147,44 @@ function generalBypassHook() {
   }, 'pointer', ['pointer', 'int', 'pointer']));
 
   // hook strstr 函数
-  Interceptor.attach(Module.findExportByName("libc.so", "strstr"), {
+  const strstrPtr = Module.findExportByName("libc.so", 'strstr');
+  Interceptor.attach(strstrPtr, {
     onEnter: function (args) {
       const keyWord = args[1].readCString();
-      if (keyWord.includes("frida") || keyWord.includes(":69A2")) {
+      if (
+        keyWord.includes("frida") ||
+        keyWord.includes(":69A2") ||
+        keyWord.includes("gum-js") ||
+        keyWord.includes("REJECT") ||
+        keyWord.includes("gmain") ||
+        keyWord.includes("gdbus") ||
+        keyWord.includes("linjector")
+      ) {
         this.isCheck = true;
       }
     },
     onLeave: function (retval) {
       if (this.isCheck) {
         retval.replace(0);
-        this.isCheck = false;
       }
     }
   });
 
-  // hook access函数
-  // access("/data/local/tmp/re.frida.server", 0)
-  Interceptor.attach(Module.findExportByName("libc.so", "access"), {
+  // hook access 函数
+  const accessPtr = Module.findExportByName("libc.so", 'access');
+  Interceptor.attach(accessPtr, {
     onEnter: function (args) {
       const path = args[0].readCString();
-      if (path.includes("re.frida.server")) {
+      if (
+        path.includes("re.frida.server") ||
+        path.includes("/data/local/tmp")
+      ) {
         this.isCheck = true;
       }
     },
     onLeave: function (retval) {
       if (this.isCheck) {
         retval.replace(-1);
-        this.isCheck = false;
       }
     },
   });
